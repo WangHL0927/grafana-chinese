16b6e26e7fc3f2147c53c8d349390094fbcb9c89
wanghualong <whl958660278@gmail.com>
2019-10-17 22:34:42 +0800

    update script.

--- tool.sh
+++ tool.sh
@@ -12,12 +12,44 @@ console() {
   esac
 }
 
+releaseDockerDev() {
+  console br
+  console info "Release Latest version and push to DockerHub"
+  console danger "Input version and press ENTER:"
+  read version
+  tag="grafana-cn:$version-dev"
+  console action "Build and push $version-dev..."
+  docker build -t $tag .
+  docker push $tag 
+  console success 'Release success!'
+
+}
+
+releaseDocker() {
+  console br
+  console info "Release Latest version and push to DockerHub"
+  console danger "Input version and press ENTER:"
+  read version
+  tag="grafana-cn:$version"
+  tag2="grafana-cn:latest"
+  console action "Build and push $version..."
+  docker build -t $tag .
+  docker push $tag
+  console action "Build and push latest..."
+  docker build -t $tag2 .
+  docker push $tag2
+  console success 'Release success!'
+}
+
+
 printMenu() {
   console br
   console info Tool Script
   console br
-  console info "1. Start dev-server"
-  console info "2. Remove dev-server"
+  console info "1. Start dev-server."
+  console info "2. Stop/Remove dev-server."
+  console info "3. Release to DockerHub with dev tag."
+  console info "4. Release latest to DockerHub."
   console br
   console danger "Input number and press ENTER:"
 }
@@ -29,6 +61,8 @@ main() {
   case $num in
   "1") ./dev-server/dev-server.sh ;;
   "2") ./dev-server/remove-dev-server.sh ;;
+  "3") releaseDockerDev ;;
+  "4") releaseDocker ;;
   *)
     console error "ERROR: undefined function!"
     exit
